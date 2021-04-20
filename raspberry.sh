d756298f39a61b1c785cba2ad0a57643116c1b99
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-04-20 18:29:45 -0300

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -9,7 +9,7 @@ bzip2 \
 ccache \
 clang \
 cmake \
-#default-jdk \
+default-jdk \
 file \
 ffmpeg \
 freeglut3-dev \
@@ -48,7 +48,7 @@ libxvidcore-dev \
 make \
 maven \
 nasm \
-#openjdk-8-jdk \
+openjdk-8-jdk \
 patch \
 perl \
 pkg-config \
