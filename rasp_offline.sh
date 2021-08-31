862709fc0f02bd60d5c008d69ca5cf971dfbd2fd
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-08-31 12:20:29 -0300

    Update rasp_offline.sh

--- rasp_offline.sh
+++ rasp_offline.sh
@@ -10,6 +10,7 @@ echo 'dtoverlay=disable-bt' | sudo tee -a /boot/config.txt
 sudo systemctl disable hciuart
 echo 'dtoverlay=disable-wifi' | sudo tee -a /boot/config.txt
 echo 'dtoverlay=i2c-gpio,i2c_gpio_sda=8,i2c_gpio_scl=9' | sudo tee -a /boot/config.txt
+echo 'tmpfs /tmp tmpfs defaults,noatime,mode=1777 0 0' | sudo tee -a /etc/fstab
 sudo systemctl stop systemd-timesyncd
 sudo systemctl disable systemd-timesyncd
 sudo systemctl disable dphys-swapfile.service
